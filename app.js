f84e926eb87b1a095e53b99fcfc872016cf801da
gZanda <gabrielfzanda@gmail.com>
2023-11-26 18:47:36 -0300

    Node Project Setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// GET Request
+app.get('/', (req, res) => {
+  res.send('Hello, your server is up and running!');
+});
+
+// GET Request
+app.get('/yey', (req, res) => {
+  res.send('yey');
+});
+
+// This defines the server port
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
