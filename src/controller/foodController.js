ab3524981b557254b4923ddd980ffdf8d532f7f1
gZanda <gabrielfzanda@gmail.com>
2023-12-06 03:06:18 -0300

    Controller Organization

---
+++ src/controller/foodController.js
@@ -0,0 +1,28 @@
+const Food = require('../database/models/Foods');
+
+// GET ALL FOODS
+const getAllFood = async (req, res) => {
+    try {
+        const foods = await Food.findAll();
+        return res.json(foods);
+    } catch (error) {
+        return res.error(error);
+    }
+}
+
+// CREATE FOOD
+const createFood = async (req, res) => {
+    try {
+        const name = req.body.name;
+        const amount = req.body.amount;
+        const food = await Food.create({ name, amount });
+        return res.json(food);
+    } catch (error) {
+        return res.error(error);
+    }
+}
+
+module.exports = {
+    getAllFood,
+    createFood
+}
--- src/router.js
+++ src/router.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Foods = require('./database/models/Foods');
-router.use(express.json());
 const {OpenAI} = require("openai");
+const foodController = require('./controller/foodController');
+router.use(express.json());
 
 const openai = new OpenAI({
     apiKey: `${process.env.OPEN_AI_KEY}`
 });
   
-
-// GET Request
-router.get('/', (req, res) => {
-    res.send('Hello, your server is up and running!');
-});
-
 // POST
-router.post('/addFood/', (req, res) => {
-    const name = req.body.name;
-    const amount = req.body.amount;
-    Foods.create({ name, amount })
-        .then(food => res.json(food))
-        .catch(error => res.json(error));
-});
+router.post('/createFood/', foodController.createFood);
 
 // GET all foods
-router.get('/foods/', (req, res) => {
-    Foods.findAll()
-        .then(foods => res.json(foods))
-        .catch(error => res.json(error));
-});
+router.get('/foods/', foodController.getAllFood);
+
+// ----------------- OPEN AI -----------------
 
 // GET Chat Response
 router.get('/ask1', async (req, res) => {
@@ -49,5 +35,7 @@ router.get('/ask2', async (req, res) => {
     });
     console.log(completion.choices[0].text);
 });
+
+// ----------------- Module Export -----------------
 
 module.exports = router;
